48a54f9a7151bc5fe23b24af9c035a433db86a09
Jordan Peralta Moscoso <jordanjpm@gmail.com>
2020-03-05 21:44:51 -0500

    minor update

--- src/app/end/end.module.ts
+++ src/app/end/end.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 // components
 import { EndComponent } from './end.component';
 import { EndLandingComponent } from './end-landing/end-landing.component';
+import { EndSummaryComponent } from './end-summary/end-summary.component';
+import { EndDetailComponent } from './end-detail/end-detail.component';
 
 // modules
 import { MaterialModule } from '../material-module';
 import { EndRoutingModule } from './end-routing.module';
-import { EndSummaryComponent } from './end-summary/end-summary.component';
-import { EndDetailComponent } from './end-detail/end-detail.component';
 
 @NgModule({
   declarations: [
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 // components
 import { HomeComponent } from './home.component';
-
-// modules
-import { HomeRoutingModule } from './home-routing.module';
 import { HomeLandingComponent } from './home-landing/home-landing.component';
 import { HomeSummaryComponent } from './home-summary/home-summary.component';
 import { HomeDetailComponent } from './home-detail/home-detail.component';
+
+// modules
+import { MaterialModule } from '../material-module';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HomeDetailComponent } from './home-detail/home-detail.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     HomeRoutingModule
   ]
 })
